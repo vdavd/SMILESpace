0a150a818f9fcf9f27ab2e5d19d7976cf396d8e6
Ville Davidsson <ville.d@hotmail.com>
2026-01-06 12:20:54 +0200

    feat: smaller file size for background image

--- backend/src/util/config.py
+++ backend/src/util/config.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-# util → src → backend
 
 TMP_DIR = BASE_DIR / "tmp_fingerprints"
 TMP_DIR.mkdir(parents=True, exist_ok=True)
